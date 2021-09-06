a07f3e0f576ddf627679181bde27bd49bcf1fad2
unknown <andre.rodrigues.he@gmail.com>
2021-09-05 21:14:35 -0300

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const api = require('./src/api')
 
 const app = express()
+app.use(cors())
+
 const port = process.env.PORT || 3000
 
 app.use(express.json())
